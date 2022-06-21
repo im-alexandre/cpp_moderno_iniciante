bf33a46790ee9a79f64cb952197db24027603929
Alexandre Castro <im-alexandre07@gmail.com>
2022-06-21 15:29:17 -0300

    Utilizando comentários em cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // Cabeçalho
+using namespace std; //Utilizando o namespace para utilizar as instruções cout e [endl]
+
+// Primeira forma de comentário
+/*
+ * Comentário
+ * com
+ * várias
+ * linhas
+ */
 
 int main(){
     cout << "Hello, world!" << '\n';
-    return 0;
+    return 0; // Toda função main deve retornar um int
 }
