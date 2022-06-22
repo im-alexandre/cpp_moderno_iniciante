d7ee04dc791ddefbcc98a2f89fd1f7e790f36b36
Alexandre Castro <im-alexandre07@gmail.com>
2022-06-22 08:47:35 -0300

    arrays compostos

--- arrays.cpp
+++ arrays.cpp
@@ -4,6 +4,10 @@ void recebe_array( int numeros[] ){
   std::cout << "A quantidade de itens do array numeros[] DENTRO DA FUNÇÃO é: " << sizeof(numeros) / sizeof(numeros[0]) << '\n';
 }
 
+void recebe_array_composto( int numeros[3][3] ){
+    numeros[1][1] = 88;
+}
+
 
 void recebe_variavel( int variavel ){
   variavel = 12;
@@ -11,7 +15,6 @@ void recebe_variavel( int variavel ){
 
 int main(){
   int numeros[] = {11, 22, 33, 44, 55};
-  
   recebe_array( numeros );
   std::cout << "A quantidade de itens do array numeros[] é: " << sizeof(numeros) / sizeof(numeros[0]) << '\n';
 
@@ -19,10 +22,11 @@ int main(){
   //std::cout << "O valor da variavel ANTES da função é: " << variavel << '\n';
   //recebe_variavel( variavel );
   //std::cout << "O valor da variavel DEPOIS da função é: " << variavel << '\n';
+
   //int multi[3][3] = { { 5, 8, 9 } , { 2, 7, 4 } , { 6, 0, 1 } };
-  //std::cout << "O valor de posição 3 ANTES da função é: " << numeros[3] << '\n';
-  //recebe_array( numeros );
-  //std::cout << "O valor de posição 3 ANTES da função é: " << numeros[3] << '\n';
+  //std::cout << "O valor de posição 3 ANTES da função é: " << multi[1][1] << '\n';
+  //recebe_array_composto( multi );
+  //std::cout << "O valor de posição 3 ANTES da função é: " << multi[1][1] << '\n';
   return 0;
 }
 
