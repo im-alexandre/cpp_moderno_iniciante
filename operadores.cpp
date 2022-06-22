b331514087373c0c928d143304951cc29eac5ce0
Alexandre Castro <im.alexandre07@gmail.com>
2022-06-21 21:35:49 -0300

    Operadores

---
+++ operadores.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int x = 6, y = 3;
+    // Operações matemáticas básicas
+    std::cout << "x + y = " << x + y << '\n';
+    std::cout << "x - y = " << x - y << '\n';
+    std::cout << "x * y = " << x * y << '\n';
+    std::cout << "x / y = " << x / y << '\n';
+    return 0;
+}
